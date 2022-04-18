439175b8e5c002d8c2e83b5adb4cf0fde7ab1366
Ilya Gornev <i.a.gornev@gmail.com>
2022-04-18 11:30:13 +0700

    Comments and logging

--- cleaner.py
+++ cleaner.py
@@ -1,12 +1,12 @@
 import subprocess
 from apscheduler.schedulers.blocking import BlockingScheduler
 
-
+# удаляет контейнер и возвращает вывод команды
 def docker_remove(id):
     cmd = f'docker rm -f {id}'
     p = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
     result = p.communicate()[0].decode("utf-8")[:-1]
-    print(result)
+    print(f'Removed: {result}')
     return result
 
 # возвращает номер последней строки, содержащей подстроку line
@@ -14,20 +14,27 @@ def find_last_line_in_logs(container, substr):
     cmd = f'''docker logs {container} 2>&1 | grep -n "{substr}" | tail --lines=1'''
     p = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
     result = p.communicate()[0].decode("utf-8")
-    lineNumber = int(result[0:result.index(':')])
-    return lineNumber
+    try:
+        lineNumber = int(result[0:result.index(':')])
+        return lineNumber
+    except ValueError:
+        return -1
 
+# возвращает список айднишников
+def get_running_containers():
+    cmd = 'docker ps -q --filter "ancestor=ride"'
+    process = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
+    result = process.communicate()[0].decode("utf-8")
+    return result.splitlines()
 
-def cleaner(container):
-    clientEnter = find_last_line_in_logs(container, "Set client")
-    clientExit = find_last_line_in_logs(container, "All contributions have been stopped")
-    if (clientExit > clientEnter):
-        print("REMOVED")
-        docker_remove(conteiner)
-
-
-conteiner = "ride"
+def cleaner():
+    for container in get_running_containers():
+        clientEnter = find_last_line_in_logs(container, "Set client")
+        clientExit = find_last_line_in_logs(container, "All contributions have been stopped")
+        print (f'Container {container}: entered {clientEnter}, exited {clientExit}')
+        if (clientExit > clientEnter):
+            docker_remove(container)
 
 scheduler = BlockingScheduler()
-scheduler.add_job(cleaner, 'interval', [conteiner], seconds=1)
+scheduler.add_job(cleaner, 'interval', seconds=1)
 scheduler.start()
